af20955bea4365ae078bac9b2c934461328a0e43
Rafeh Qazi <qazi@cleverprogrammer.com>
2020-12-29 10:46:13 -0800

    add click counter using react hooks and functional componenet

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-// 1. import react & reactdom ✅
-import React from 'react';
+// 1. import useState hook from react
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-// 2. make a react component (function or class)
+// 2. make a functional react component that has a button & a counter
 const App = function() {
+    // how to write `const counter = 0` in ReactJS 👇
+    const [counter, setCounter] = useState(0);
 
     return (
     <div>
       <h2>Let's go Daniel 💥🔫!</h2>
+      {/* increment counter by 1 every time you click the button */}
+      <button onClick={() => setCounter(counter + 1)} >Click Me 👉 {counter}</button>
     </div>
     )
 };
 
 // 3. show it on the screen 🚀
 ReactDOM.render(<App />, document.getElementById('root'))
+
+// 2-click-counter-with-react-hooks-functional
